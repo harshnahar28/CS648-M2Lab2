3701b34bdb6cdba9be5ec6b2c8f6238bbf8b1897
harshnahar28 <103315043+harshnahar28@users.noreply.github.com>
2023-02-09 06:27:38 -0800

    changes reflected

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ window.addEventListener("load",()=>
 {
     "use strict";   
 
-    window.document.getElementById("empForm").addEventListener("submit", (e)=> 
+    window.document.getElementById("formForEmployee").addEventListener("submit", (e)=> 
     {
         e.preventDefault();
-        window.console.log("The ID is " + window.document.getElementById("id").value);
-        window.console.log("The Name is " + window.document.getElementById("name").value);
-        window.console.log("The Extension is " + window.document.getElementById("ext").value);
-        window.console.log("The Email is " + window.document.getElementById("email").value);
-        window.console.log("The Department is" + window.document.getElementById("department").value);      
+        window.console.log("The ID is " + window.document.getElementById("ID").value);
+        window.console.log("The Name is " + window.document.getElementById("Name").value);
+        window.console.log("The Extension is " + window.document.getElementById("Extension").value);
+        window.console.log("The Email is " + window.document.getElementById("Email").value);
+        window.console.log("The Department is" + window.document.getElementById("Department").value);      
     });
   });
 
