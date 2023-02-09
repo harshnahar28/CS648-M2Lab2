92dc10fc3638cdabeb2897495309949ba52e0c1f
harshnahar28 <103315043+harshnahar28@users.noreply.github.com>
2023-02-09 06:17:14 -0800

    changes reflected

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load",()=>
+{
     "use strict";   
-    window.document.getElementById("empForm").addEventListener("submit", (e)=> {
+
+    window.document.getElementById("empForm").addEventListener("submit", (e)=> 
+    {
         e.preventDefault();
-        window.console.log("ID:" + window.document.getElementById("id").value);
-        window.console.log("Name:" + window.document.getElementById("name").value);
-        window.console.log("Extension:" + window.document.getElementById("ext").value);
-        window.console.log("Email:" + window.document.getElementById("email").value);
-        window.console.log("Department:" + window.document.getElementById("department").value);      
+        window.console.log("The ID is " + window.document.getElementById("id").value);
+        window.console.log("The Name is " + window.document.getElementById("name").value);
+        window.console.log("The Extension is " + window.document.getElementById("ext").value);
+        window.console.log("The Email is " + window.document.getElementById("email").value);
+        window.console.log("The Department is" + window.document.getElementById("department").value);      
     });
   });
+
+/* You can check this output in the console in a browser. */
