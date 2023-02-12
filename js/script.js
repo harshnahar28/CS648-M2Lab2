371976f94450911450e6dc508574287c690467ca
harshnahar28 <103315043+harshnahar28@users.noreply.github.com>
2023-02-12 09:35:45 -0800

    changes reflected

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ window.addEventListener("load",()=>
 {
     "use strict";   
 
-    window.document.getElementById("formForEmployee").addEventListener("submit", (e)=> 
+    window.document.getElementById("formForEmployee").addEventListener("submit", (obj)=> 
     {
-        e.preventDefault();
-        window.console.log("The ID is " + window.document.getElementById("ID").value);
-        window.console.log("The Name is " + window.document.getElementById("Name").value);
-        window.console.log("The Extension is " + window.document.getElementById("Extension").value);
-        window.console.log("The Email is " + window.document.getElementById("Email").value);
-        window.console.log("The Department is " + window.document.getElementById("Department").value);      
+        obj.preventDefault();
+        window.console.log("The ID is " + window.document.getElementById("id").value);
+        window.console.log("The Name is " + window.document.getElementById("name").value);
+        window.console.log("The Extension is " + window.document.getElementById("extension").value);
+        window.console.log("The Email is " + window.document.getElementById("email").value);
+        window.console.log("The Department is " + window.document.getElementById("department").value);      
     });
   });
 
-/* You can check this output in the console in a browser. */
+/* Using arrow functions, you can check this output in the console in a browser. */
